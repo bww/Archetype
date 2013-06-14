b034942ed9197a8bc4bf8fd386ccc793c4a1e91d
Brian William Wolter <brian@woltergroup.net>
2013-06-14 18:00:22 -0400

    Initial

---
+++ Archetype/source/archetype/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Archetype
+//
+//  Created by Brian William Wolter on 6/14/13.
+//  Copyright (c) 2013 Brian William Wolter. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+  @autoreleasepool {
+      
+      // insert code here...
+      NSLog(@"Hello, World!");
+      
+  }
+    return 0;
+}
+
